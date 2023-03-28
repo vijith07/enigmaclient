cc009c9a0ce3ca85be20b1660316616235ea2493
Vijith Kusumba <45901766+vijith07@users.noreply.github.com>
2023-03-28 08:05:19 +0530

    create project

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+
+  <div class="text-3xl border bg-slate-700 text-white p-4">
+    My SvelteKit App
+  </div>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
